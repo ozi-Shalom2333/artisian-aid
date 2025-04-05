173b9ed504bab7fe1190d9445748b78df41b4a93
you@example.com <test\2gmail.com>
2025-04-05 19:56:59 +0100

    more coding

--- src/pages/CategoryByFilter.jsx
+++ src/pages/CategoryByFilter.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const CategoryByFilter = () => {
+   const {category} = useParams()
   return (
     <div>
+      axios.get(`url/getone/${category}`)
       serfthujmk,l
     </div>
   )

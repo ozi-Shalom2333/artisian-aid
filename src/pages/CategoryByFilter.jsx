b2815445e6a53cba4be89c813aade64578ede80c
Bigjosh751 <you@example.com>
2025-04-05 08:52:20 +0100

    category by filiter alomst done

--- src/pages/CategoryByFilter.jsx
+++ src/pages/CategoryByFilter.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import './CategoryByFiliter.css'
 
-const CategoryByFilter = () => {
+const CategoryByFiliter = () => {
   return (
-    <div>
-      serfthujmk,l
+    <div className="cat-hero">
+        <img src="https://s3-alpha-sig.figma.com/img/aa01/ae37/f9348ed1d380e33d8c556a6bcafc47e4?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ITsl1j~zUlO1cRcHL8j83qpjUAB8iWx09LOkUjP8lWG-5yb~CQaJjmk3CZsZqnUEHdMrntgTmjznTJ3wnkXG4WT751GzXJhFOxG7XUKX7GJ8SXNtIh9NsjeShnFOF9mLskZ7WNkcFyUAGFuJczacfLb26QIRglLxIc-u61G9aPQU9fPRJC3mOTuMK4Th64tPKif7sroHapC4D4i5EkzsJwjLk0JDaAQSiCkMFKjocpqUWUZYbgjbEeS8Xu1CKbhqJMFNtY5m0Wqh8sYIbNr32V7cb5PGQKAhTcYpiuUlsazJc~yxIjrhmYrygyLop6NUuOSgZELPmwZsTS974TgePw__" alt="" />
+    <div className="cat-overlay">
+      <p className="cat-subheading">Painting Services</p>
+      <h1>
+        Get <span className="highlight">Satisfying</span> Painting <br />
+        Services at your Finger Tip
+      </h1>
     </div>
+  </div>
   )
 }
 
-export default CategoryByFilter
+export default CategoryByFiliter

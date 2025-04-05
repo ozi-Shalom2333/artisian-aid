3b29a4b0a072d28951df7ed7b23be5996794ed73
Gerrard-Elibe <gerardelibe@gmail.com>
2025-04-05 22:47:52 +0100

    Initial commit

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import { FaBolt, FaHammer, FaLeaf, FaPaintBrush, FaBroom } from "react-icons/fa";  
+import "./CategoryPage.css";
 
 const CategoryPage = () => {
   return (
-    <div>
-      <h1>Category page</h1>
-    </div>
-  )
-}
+    <div className="category-page-container">
+      <div className="overlay-background"></div>
 
-export default CategoryPage
+  
+      <div className="category-title-section">
+        <h1 className="category-title">
+          Discover Our <span className="highlight-text">Artisans</span> Categories
+        </h1>
+        <p className="category-description">
+          Browse our categories to find the perfect professional for your needs.
+        </p>
+      </div>
+
+     
+      <div className="category-item-boxes">
+        <div className="category-item-box">
+          <FaBolt className="category-icon" /> 
+          <span className="category-label">Electrician</span>
+        </div>
+        <div className="category-item-box">
+          <FaHammer className="category-icon" />
+          <span className="category-label">Carpentry</span>
+        </div>
+        <div className="category-item-box">
+          <FaLeaf className="category-icon" /> 
+          <span className="category-label">Gardening</span>
+        </div>
+        <div className="category-item-box">
+          <FaPaintBrush className="category-icon" /> 
+          <span className="category-label">Painting</span>
+        </div>
+        <div className="category-item-box">
+          <FaBroom className="category-icon" /> 
+          <span className="category-label">Home Cleaning</span>
+        </div>
+        <div className="category-item-box">
+          <FaBolt className="category-icon" /> 
+          <span className="category-label">Electrician</span>
+        </div>
+        <div className="category-item-box">
+          <FaHammer className="category-icon" />
+          <span className="category-label">Carpentry</span>
+        </div>
+        <div className="category-item-box">
+          <FaLeaf className="category-icon" />
+          <span className="category-label">Gardening</span>
+        </div>
+        <div className="category-item-box">
+          <FaPaintBrush className="category-icon" /> 
+          <span className="category-label">Painting</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;

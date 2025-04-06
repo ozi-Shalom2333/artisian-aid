2a0b16a33b8973fa0d54ecb059ec3428debb1c3c
you@example.com <test\2gmail.com>
2025-04-06 07:12:47 +0100

    omo 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './routes/Router'
-import FooterSub from './components/FooterSub'
+
 
 
 createRoot(document.getElementById('root')).render(

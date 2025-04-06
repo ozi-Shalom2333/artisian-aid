4d1101446c46d43270465beb4bdf3ee7b1d9d3b1
you@example.com <test\2gmail.com>
2025-04-06 06:59:23 +0100

    omo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
-import FooterSub from './components/FooterSub'
+
 
 const App = () => {
   return (
@@ -10,7 +10,6 @@ const App = () => {
       <Header />
       {/* <main style={{ height: 'calc(100vh - 200px)', backgroundColor: '#f0f0f0' }}> */}
         <Outlet />
-        <FooterSub/>
       <Footer />
     </div>
   )

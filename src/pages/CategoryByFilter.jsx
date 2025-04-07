50a1117dd3ce3656699918c517c1b64715ccab35
Bigjosh751 <you@example.com>
2025-04-07 15:47:12 +0100

    corrected

--- src/pages/CategoryByFilter.jsx
+++ src/pages/CategoryByFilter.jsx
@@ -1,77 +1,46 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios"; 
 import "./CategoryByFiliter.css";
 import { VscStarFull } from "react-icons/vsc";
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import { RiVerifiedBadgeFill } from "react-icons/ri";
-import { ToastContainer, toast } from "react-toastify"; 
-import "react-toastify/dist/ReactToastify.css"; 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const CategoryByFiliter = () => {
-  // const artisans = [
-  //   {
-  //     id: 1,
-  //     name: "Tolu Andy",
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdIPLcEdf9OJ2iCtCc9ZdrxmGyiczKQEN7mw&s",
-  //     service: "Painter Service",
-  //     rating: 5,
-  //     verified: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Dave Odi",
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAenRG0CI1bP-jPWSoXlX6hR-AOLDzhfsFRQ&s",
-  //     service: "Painter Service",
-  //     rating: 5,
-  //     verified: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Dave Odi",
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAenRG0CI1bP-jPWSoXlX6hR-AOLDzhfsFRQ&s",
-  //     service: "Painter Service",
-  //     rating: 5,
-  //     verified: true,
-  //   },
-  // ];
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [category, setCategory] = useState(""); 
+
   const renderStars = (rating) => {
-    if(rating===0){
+    if (rating === 0) {
       return null;
     }
     const stars = [];
     for (let i = 0; i < rating; i++) {
-      stars.push(<VscStarFull key={i} style={{ color: "#FFD700" }}/>);
+      stars.push(<VscStarFull key={i} style={{ color: "#FFD700" }} />);
     }
     return stars;
   };
 
-
-
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [category, setCategory] = useState(""); // Default category
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `https://artisanaid.onrender.com/v1/artisans/category?category=${category}`
+        const response = await axios.get(
+          `https://artisanaid.onrender.com/v1/artisans/category`,
+          {
+            params: { category },
+          }
         );
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        if (response.status === 404) {
-          setError("No results found for this category.");
-          return;
-        }
-        const data = await response.json();
-        console.log(data);
-        setItems(data.data);
-        console.log(data.data);
+        setItems(response.data.data); 
+        console.log(response.data.data);
       } catch (err) {
-        setError(err.message);
+        if (err.response && err.response.status === 404) {
+          setError("No results found for this category.");
+        } else {
+          setError(err.message);
+        }
       } finally {
         setLoading(false);
       }
@@ -101,64 +70,68 @@ const CategoryByFiliter = () => {
       </div>
       <div className="artisan-container">
         <div className="search-bar">
-          <input type="text" placeholder="Search ..." />
+          <input
+            type="text"
+            placeholder="Search ..."
+            onChange={(e) => setCategory(e.target.value)} 
+          />
           <button className="search-btn">Search</button>
         </div>
 
         <div className="card-grid">
-          {items.length > 0
-            ? items.map((items) => (
-                <div className="card" key={items.id}>
-                  <div className="card-header">
-                    <div className="profile-info">
-                      <img
-                        className="profile-img"
-                        src={items.image}
-                        alt={items.fullname}
-                      />
-                      <div>
-                        <p className="name">
-                          {items.name}{" "}
-                          {items.isVerified && (
-                            <span className="check">
-                              <RiVerifiedBadgeFill />
-                            </span>
-                          )}
-                        </p>
-                        <a href="#" className="view-profile">
-                          View profile
-                        </a>
-                      </div>
+          {loading ? (
+            <p>Loading...</p>
+          ) : items.length > 0 ? (
+            items.map((items) => (
+              <div className="card" key={items.id}>
+                <div className="card-header">
+                  <div className="profile-info">
+                    <img
+                      className="profile-img"
+                      src={items.image}
+                      alt={items.fullname}
+                    />
+                    <div>
+                      <p className="name">
+                        {items.name}{" "}
+                        {items.isVerified && (
+                          <span className="check">
+                            <RiVerifiedBadgeFill />
+                          </span>
+                        )}
+                      </p>
+                      <a href="#" className="view-profile">
+                        View profile
+                      </a>
                     </div>
-                    <a href="#" className="expand">
-                      <BsBoxArrowUpRight />
-                    </a>
                   </div>
-
-                  <img
-                    src={items.image}
-                    alt="s"
-                    className="service-img"
-                  />
-
-                  <div className="card-footer">
-                    <p className="service-name">{items.category}</p>
-                    <p className="rating">
-                  {renderStars(items.rating)}
-                  {items.isRecommended &&  <span className="recommend">
-                       {items.isRecommended}
-                      </span>}
-                    </p>
-                    <button
-                      className="book-btn"
-                      onClick={() => handleBook(items.fullname)}
-                    >
-                      Book Now
-                    </button>
-                  </div>
+                  <a href="#" className="expand">
+                    <BsBoxArrowUpRight />
+                  </a>
                 </div>
-              ))
-            : error}
+
+                <img src={items.image} alt="s" className="service-img" />
+
+                <div className="card-footer">
+                  <p className="service-name">{items.category}</p>
+                  <p className="rating">
+                    {renderStars(items.rating)}
+                    {items.isRecommended && (
+                      <span className="recommend">{items.isRecommended}</span>
+                    )}
+                  </p>
+                  <button
+                    className="book-btn"
+                    onClick={() => handleBook(items.fullname)}
+                  >
+                    Book Now
+                  </button>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>{error}</p>
+          )}
         </div>
       </div>
 

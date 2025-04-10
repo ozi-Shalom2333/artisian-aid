adcd067209f5e8f1dbc45fb578b2d5dda6f337c6
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-10 20:07:10 +0100

    Update Login.jsx

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
     }
   };
 
-  const handleForgotPassword = () => navigate('/forgot-password');
+  const handleForgotPassword = () => navigate('/forget');
   const handleSignUpRedirect = () => navigate('/signup');
 
   return (

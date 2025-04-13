c5cbf8c8e681fe11a86cc2a1d5b7f00ba6151687
you@example.com <test\2gmail.com>
2025-04-13 22:45:08 +0100

    fine tuned dashboard

--- src/dashboards/admin/AdminDashboard.jsx
+++ src/dashboards/admin/AdminDashboard.jsx
@@ -1,64 +1,84 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
 import '../../styles/admindashboard.css'
 import PendingVerification from './pages/PendingVerification';
 import ApprovedUsers from './pages/ApprovedUsers';
 import DeclinedUsers from './pages/DeclinedUsers';
 import ReportedUsers from './pages/ReportedUsers';
-
-
+import { RiHomeFill } from "react-icons/ri";
 
 const AdminDashboard = () => {
+
+  const [activeTab, setActiveTab] = useState('/pending-verification'); // Initialize with the default active tab
+
   return (
     <Router>
-    <div className="dashboard-container">
+
+      <div className="dashboard-container">
       <div className="sidebar">
-        <div className="logo">ArtisanAid.</div>
-        <nav>
-          <ul>
-            <li className="admin-management-header">
-      
+          <div className="logo">ArtisanAid.</div>
+          <nav>
+            <ul>
+              <li className="admin-management-header">
+                <RiHomeFill size={20}/>
                 Admin Management
-                </li>
-            <li>
-              <Link to="/pending-verification" className="nav-link">
-                Pending User Verification
-              </Link>
-            </li>
-            <li>
-              <Link to="/approved-users" className="nav-link">
-                Approved Users
-              </Link>
-            </li>
-            <li>
-              <Link to="/declined-users" className="nav-link">
-                Declined Users
-              </Link>
-            </li>
-            <li>
-              <Link to="/reported-users" className="nav-link">
-                Reported Users
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="logout">
-          <Link to="/logout" className="nav-link">
-            Log out
-          </Link>
+              </li>
+              <li>
+                <NavLink
+                  to="/pending-verification"
+                  className={activeTab === '/pending-verification' ? 'nav-link-active' : 'nav-link'}
+                  onClick={() => setActiveTab('/pending-verification')}
+                >
+                  Pending User Verification
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/approved-users"
+                  className={activeTab === '/approved-users' ? 'nav-link-active' : 'nav-link'}
+                  onClick={() => setActiveTab('/approved-users')}
+                >
+                  Approved Users
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/declined-users"
+                  className={activeTab === '/declined-users' ? 'nav-link-active' : 'nav-link'}
+                  onClick={() => setActiveTab('/declined-users')}
+                >
+                  Declined Users
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/reported-users"
+                  className={activeTab === '/reported-users' ? 'nav-link-active' : 'nav-link'}
+                  onClick={() => setActiveTab('/reported-users')}
+                >
+                  Reported Users
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          {/* <div className="logout">
+            <NavLink to="/logout" className="nav-link">
+              Log out
+            </NavLink>
+          </div> */}
+        </div>
+        <div className="main-content">
+          <Routes>
+            <Route path="/pending-verification" element={<PendingVerification/>} />
+            <Route path="/approved-users" element={<ApprovedUsers />} />
+            <Route path="/declined-users" element={<DeclinedUsers/>} />
+            <Route path="/reported-users" element={<ReportedUsers />} />
+            <Route path="/" element={<PendingVerification />} />
+          </Routes>
         </div>
       </div>
-      <div className="main-content">
-        <Routes>
-          <Route path="/pending-verification" element={<PendingVerification/>} />
-          <Route path="/approved-users" element={<ApprovedUsers />} />
-          <Route path="/declined-users" element={<DeclinedUsers/>} />
-          <Route path="/reported-users" element={<ReportedUsers />} />
-          <Route path="/" element={<PendingVerification />} />
-        </Routes>
-      </div>
-    </div>
-  </Router>
+    </Router>
   )
 }
 
--- src/dashboards/admin/pages/ApprovedUsers.jsx
+++ src/dashboards/admin/pages/ApprovedUsers.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../../../styles/approvedusers.css'
 
 const ApprovedUsers = () => {
   return (
-    <div>
-      
+    <div className='approved-users-container'>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, facere.</h1>
     </div>
   )
 }

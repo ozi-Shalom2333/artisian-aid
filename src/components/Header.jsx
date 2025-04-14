35913ae6f5cefd21e5c2b3b90969cfc93f9de4be
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-14 22:46:43 +0100

    touched the responsivness (#64)
    Co-authored-by: Gerrard-Elibe <gerardelibe@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import './../styles/header.css'
 
 const Header = () => {
+    
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen)
+    }
     const navigate = useNavigate()
   return (
     <div className='Header-container'>
       <section className='Header-section'>
-      <img onClick={()=> navigate('/')} style={{objectFit: 'contain', width: '15%', cursor:'pointer'}} src="https://res.cloudinary.com/djnowfvsk/image/upload/v1744194748/Artisan_ifmbb4.png" alt=""  sizes='30'/>
-      
+        <div className='responsive-image'> 
+          <img onClick={()=> navigate('/')} src="https://res.cloudinary.com/dffl7wbtb/image/upload/v1744652328/Artisan_4_tzas3g.png" alt="" />
+        </div>
+
+        <div className='Header-Pages'>
+          <p>
+            <NavLink to="/" end >Home</NavLink>
+          </p>
+          <p>
+            <NavLink to="/about" >About Us</NavLink>
+          </p>
+          <p>
+          <NavLink to="/contact">Contact us</NavLink>
+          </p>
+        </div>
 
 
-      <div className='Header-Pages'>
-        <p>
-          <NavLink to="/" end >Home</NavLink>
-        </p>
-        <p>
-          <NavLink to="/about" >About Us</NavLink>
-        </p>
-        <p>
-         <NavLink to="/contact">Contact us</NavLink>
-        </p>
-      </div>
-
-
-      <div className='Header-authentication'>
-        <p>
-            <NavLink to="/authoption" >Sign Up</NavLink>
-        </p>
-        <p>
-            <NavLink to="/login" >Login</NavLink>
-        </p> 
+        <div className='Header-authentication'>
+          <p>
+              <NavLink to="/authoption" >Sign Up</NavLink>
+          </p>
+          <p>
+              <NavLink to="/login" >Login</NavLink>
+          </p> 
         
-      </div>
+        </div>
+        <div  className='second-respon' onClick={toggleDropdown}>
+          <img src="https://res.cloudinary.com/dffl7wbtb/image/upload/v1744652397/Component_64_vvm2yd.png" alt="" />
+        </div>
+        {
+          isOpen == true ? 
+            <div className='dropdown' >
+              <p>Home</p>
+              <p>About Us</p>
+              <p>Contact Us</p>
+              <p>Login</p>
+              <p>Sign Up</p>
+            </div> 
+          : null
+        }
       </section>
     </div>
   )

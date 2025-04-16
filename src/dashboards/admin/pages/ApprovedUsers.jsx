725e64c4aee8c6a0d245668e7e9736522185d497
Vickypearl008 <trustvictoria505@gmail.com>
2025-04-16 11:23:54 +0100

    Emem (#69)
    * if needed
    
    * cant wait for this to be over
    
    ---------
    
    Co-authored-by: ozi-Shalom2333 <okaozisol45@gmail.com>

--- src/dashboards/admin/pages/ApprovedUsers.jsx
+++ src/dashboards/admin/pages/ApprovedUsers.jsx
@@ -4,7 +4,40 @@ import '../../../styles/approvedusers.css'
 const ApprovedUsers = () => {
   return (
     <div className='approved-users-container'>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, facere.</h1>
+      <aside className='approveHeader'>
+        <nav className='headerHolder'>
+          <h5>Welcome back, Admin</h5>
+          <p>here is your dashboard</p>
+        </nav>
+      </aside>
+
+      <section className='adminDashboard'>
+        <div className='pendingUser'>
+          <h5>Pending User Verification</h5>
+        </div>
+        <aside className='adminDashboardMainContainer'>
+          <nav className='adaezeJane'>
+            <div className='adaezeImage'>
+              <img src="/fine girl.jpg" alt="" />
+              <p>Adaeze Jane</p>
+            </div>
+            
+            <div className='adaezeEmail'>
+              <p>Adaezejane2025@gmail.com</p>
+            </div>
+            <div className='adaezeDetails'>
+              <span>View details</span>
+            </div>
+
+            <div className='adaStatus'>
+              <p>Status: Approved</p>
+
+            </div>
+          </nav>
+        </aside>
+
+      </section>
+
     </div>
   )
 }

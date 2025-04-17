dc6e8143089d6057b36b506e897c4d5ec56ea431
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-17 16:38:51 -0700

    Update PendingUserCard.jsx

--- src/components/PendingUserCard.jsx
+++ src/components/PendingUserCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const PendingUserCard = ({ user, onViewDetails }) => {
   return (
@@ -16,13 +15,5 @@ const PendingUserCard = ({ user, onViewDetails }) => {
   );
 };
 
-PendingUserCard.propTypes = {
-  user: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-  }).isRequired,
-  onViewDetails: PropTypes.func.isRequired,
-};
 
 export default PendingUserCard;

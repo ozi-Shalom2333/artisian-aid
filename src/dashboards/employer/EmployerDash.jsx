09adf4ea81bb9ff6c791d67c43c516599d9b7d9e
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-17 09:18:27 -0700

    na to mad remain

--- src/dashboards/employer/EmployerDash.jsx
+++ src/dashboards/employer/EmployerDash.jsx
@@ -16,9 +16,6 @@ const EmployerDash = () => {
      const handleTabClick = (tab) => {
        setActiveTab(tab);
      };
-    
-  
-
      return (
        <div className="dashboard-container">
         

fd4df7e1c422ad7e4377d428017414af013719aa
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-18 08:52:46 -0700

    finish card components (#93)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,17 @@ import './../styles/profile.css';
 import { AiFillStar } from 'react-icons/ai'; 
 import { MdVerified } from "react-icons/md";
 
-function Card({data}) {
+function Card({data, onClick}) {
   return (
-    <div className="card">
+    <div className="card" onClick={onclick}>
      <section className='card-header'>
         <div>
           <header>
-            <img src="https://res.cloudinary.com/dd1aj3hvn/image/upload/v1744935876/Frame_4_ke3z11.png" alt="" />
+            <img src={data.profilePic?.image_url || "https://res.cloudinary.com/dd1aj3hvn/image/upload/v1744935876/Frame_4_ke3z11.png"} alt={data.fullname} />
           </header>
           <aside>
             <h3>
-              Adaeze Jane
+              {data.fullname}
               <MdVerified size={20} color='blue'/>
             </h3>
             <p><u>view profile</u></p>
@@ -24,19 +24,19 @@ function Card({data}) {
         </div>
       </section>
       <section className='coverpic'>
-        <img src="https://res.cloudinary.com/dd1aj3hvn/image/upload/v1744947459/Card_pics_1_to10t1.png" alt="" />
+        <img src={data.jobPostId.jobImage?. Image_url || "https://res.cloudinary.com/dd1aj3hvn/image/upload/v1744947459/Card_pics_1_to10t1.png"} alt="" />
       </section>
       <section className='fancy'>
         <div>
-        <h1>Carpenter Service</h1>
+        <h1>{data.category || services}</h1>
         <div className="service-details">
-           <span className="rating">
+           {/* <span className="rating">
            <AiFillStar size={10}  color='yellow'/>
            <AiFillStar size={10} color='yellow'/>
            <AiFillStar size={10} color='yellow'/>
            <AiFillStar size={10} color='yellow'/>
            <AiFillStar size={10} color='yellow'/>
-         </span>
+         </span> */}
          <span className="recommended">Recommended</span> 
         </div>
         </div>

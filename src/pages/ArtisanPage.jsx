517aec63bc900c519ed8758fc5fed7f98191ac8d
Gerrard-Elibe <gerardelibe@gmail.com>
2025-04-19 22:43:25 +0100

    Privacy (#107)
    * Merge branch 'master' of https://github.com/ozi-Shalom2333/artisian-aid into gerrard-kelechi-elibe
    
    * privacy
    
    * cat
    
    * Merge branch 'master' of https://github.com/ozi-Shalom2333/artisian-aid into Privacy
    
    * userprofile

--- src/pages/ArtisanPage.jsx
+++ src/pages/ArtisanPage.jsx
@@ -36,7 +36,6 @@ const ArtisanPage = () => {
         <h1 className="Ahero-title">"Uncover <span style={{color:'rgba(47, 128, 237, 1)'}}>Unique</span> Talents and{" "}<br />Exceptional Skills"</h1>
         <p className="Ahero-description">Explore the Artisan Community: Where Creativity Thrives</p>
       </div>
-      const navigate = useNavigate();
 
 <div className="ozioma">
   {artisans.map((data, onclick) => (

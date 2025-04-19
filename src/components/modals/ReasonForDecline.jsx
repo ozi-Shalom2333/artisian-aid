b95089a5d98f3261cb920fca199cd7be573bf972
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-19 16:13:19 -0700

    Header refine (#109)
    * corrected coverpic to profilepic
    
    * header updated
    
    * header refine
    
    * Header Refined
    
    * Merge branch 'master' of https://github.com/ozi-Shalom2333/artisian-aid into HeaderRefine
    
    * cleared all the css

--- src/components/modals/ReasonForDecline.jsx
+++ src/components/modals/ReasonForDecline.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import '../../styles/ReasonForDecline.css'; // Import the CSS file
+import { useNavigate } from 'react-router-dom'; 
+import '../../styles/ReasonForDecline.css'; 
 
 const ReasonForDecline = ({ onSubmit }) => {
   const [reason, setReason] = useState('');
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate(); 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/declined-users'); // Route to the declinedUsers page
+    navigate('/declined-users'); 
   };
 
   return (
-    <div className="modal-overlay">
-      <div className="modal-container">
-        <h2 className="modal-title">Why decline user?</h2>
+    <div className="modal-overlay1">
+      <div className="modal-container1">
+        <h2 className="modal-title1">Why decline user?</h2>
         <form onSubmit={handleSubmit}>
           <textarea
             value={reason}
             onChange={(e) => setReason(e.target.value)}
-            className="modal-textarea"
+            className="modal-textarea1"
             placeholder="Enter reason for declining"
           ></textarea>
-          <button type="submit" className="modal-submit-btn">
+          <button type="submit" className="modal-submit-btn1">
             Submit
           </button>
         </form>
@@ -35,40 +35,3 @@ export default ReasonForDecline;
 
 
 
-
-
-
-
-
-// import React, { useState } from 'react';
-// import '../../styles/ReasonForDecline.css'; // Import the CSS file
-
-// const ReasonForDecline = ({ onSubmit }) => {
-//   const [reason, setReason] = useState('');
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     onSubmit(reason);
-//   };
-
-//   return (
-//     <div className="modal-overlay">
-//       <div className="modal-container">
-//         <h2 className="modal-title">why decline decline user?</h2>
-//         <form onSubmit={handleSubmit}>
-//           <textarea
-//             value={reason}
-//             onChange={(e) => setReason(e.target.value)}
-//             className="modal-textarea"
-//             placeholder=""
-//           ></textarea>
-//           <button type="submit" className="modal-submit-btn">
-//             Submit
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ReasonForDecline;

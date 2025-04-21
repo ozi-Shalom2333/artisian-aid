bcc554c4b51a9d993dca726a056dbdecb716df72
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-21 16:10:12 -0700

    catch error message

--- src/auth/VerifyEmail.jsx
+++ src/auth/VerifyEmail.jsx
@@ -48,15 +48,8 @@ const VerifyEmail = () => {
     navigate('/login');
   };
 
-  if (isLoading) {
-    return (
-      <div className="loadingModal">
-        <p>Verifying your account...</p>
-      </div>
-    );
-  } else{
-    null
-  }
+  if (!isVerified) return null;
+
 
  
   return (

897a305d07cc17c952cdea790fb2992de799565d
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-04-21 20:38:55 -0700

    some adjustments

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/categorypage.css"
+import "../styles/categorypage.css";
 import Header from "../components/Header";
+import categoryListing from "../components/categoryList"; 
 
 const CategoryPage = () => {
-  
   const navigate = useNavigate();
- 
-  
+
+  const handleCategoryClick = (category) => {
+    navigate(`/category/${category}`);
+  };
 
   return (
-    <div className="category-page-container">  
-    <Header/>
+    <div className="category-page-container">
+      <Header />
       <div className="category-title-section">
         <h1 className="category-title">
           Discover Our <span className="highlight-text">Artisans</span> Categories
@@ -21,32 +23,21 @@ const CategoryPage = () => {
         </p>
       </div>
 
-     
       <div className="category-item-boxes">
-        <div className="category-item-box-G" onClick={()=> navigate('/category/electrician')}>
-          <img src="https://res.cloudinary.com/djnowfvsk/image/upload/v1744220125/electrician-svgrepo-com_1_u0mzzu.png" className="category-icon" /> 
-          <span className="category-label">Carpentry Services</span>
-        </div>
-        <div className="category-item-box-G" onClick={()=> navigate('/category/gardening')}>
-          <img src="https://res.cloudinary.com/djnowfvsk/image/upload/v1744220126/gardening-tools-equipment-svgrepo-com_1_1_sebwge.png" className="category-icon" /> 
-          <span className="category-label">Gardening Services</span>
-        </div>
-        <div className="category-item-box-G" onClick={()=> navigate('/category/laundry')}>
-          <img src="https://res.cloudinary.com/djnowfvsk/image/upload/v1744220130/Vector_2_zcermi.png" className="category-icon" /> 
-          <span className="category-label">Laundary Services</span>
-        </div>
-        <div className="category-item-box-G" onClick={()=> navigate('/category/home cleaning')}> 
-          <img src="https://res.cloudinary.com/djnowfvsk/image/upload/v1744220513/cleaner-broom-svgrepo-com_1_bcq4ca.png" className="category-icon" /> 
-          <span className="category-label">Home Cleaning Services</span>
-        </div>
-        <div className="category-item-box-G" onClick={()=> navigate('/category/electrician')}>
-          <img src="https://res.cloudinary.com/djnowfvsk/image/upload/v1744220125/electrician-svgrepo-com_1_u0mzzu.png" className="category-icon" /> 
-          <span className="category-label">Electrical Services</span>
-        </div>
-        <div className="category-item-box-G" onClick={()=> navigate('/category/pain')}>
-        <img src="https://res.cloudinary.com/djnowfvsk/image/upload/v1744220129/painter-with-roller-and-paint-bucket-svgrepo-com_1_aaeeep.png" className="category-icon" />
-        <span className="category-label">Painting Services</span>
-        </div>
+        {categoryListing.map((category, index) => (
+          <div
+            key={index}
+            className="category-item-box-G"
+            onClick={() => handleCategoryClick(category.text)}
+          >
+            <img
+              src={category.image}
+              className="category-icon"
+              alt={`${category.text} icon`}
+            />
+            <span className="category-label">{category.text}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
